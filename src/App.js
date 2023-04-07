35a4515beee5e8d1224f2ce1da1f5fcea3514f36
lisachenko_roman <lisachenkoroma2009@gmail.com>
2023-04-07 21:49:11 +0300

    добавлен блок UserLogIn

--- src/App.js
+++ src/App.js
@@ -10,34 +10,36 @@ import Question from "./components/Questions/Question"
 import Cooperation from "./components/Cooperation/Cooperation"
 import InvitationToCooperation from "./components/InvitationToCooperation/InvitationToCooperation"
 import Footer from "./components/Footer/Footer"
+import UserLogIn from "./components/UserLogIn/UserLogIn";
 
 function App() {
     return (
         <div className="App">
-            <div className='wrapper'>
-                <header className='header'>
-                    <Navbar />
-                    <div className='searchContainer'>
-                        <Search />
-                        <User />
-                    </div>
-                </header>
-                    <main>
-                        <Greetings />
-                        <Why />
-                        <div className='circle'></div>
-                        <Stock />
-                        <Reviews />
-                        <Question />
-                        <Cooperation />
-                        <InvitationToCooperation />
-                    </main>
-            </div>
-            <footer>
-                <div className='wrapper'>
-                    <Footer />
-                </div>
-            </footer>
+            {/*<div className='wrapper'>*/}
+            {/*    <header className='header'>*/}
+            {/*        <Navbar />*/}
+            {/*        <div className='searchContainer'>*/}
+            {/*            <Search />*/}
+            {/*            <User />*/}
+            {/*        </div>*/}
+            {/*    </header>*/}
+            {/*        <main>*/}
+            {/*            <Greetings />*/}
+            {/*            <Why />*/}
+            {/*            <div className='circle'></div>*/}
+            {/*            <Stock />*/}
+            {/*            <Reviews />*/}
+            {/*            <Question />*/}
+            {/*            <Cooperation />*/}
+            {/*            <InvitationToCooperation />*/}
+            {/*        </main>*/}
+            {/*</div>*/}
+            {/*<footer>*/}
+            {/*    <div className='wrapper'>*/}
+            {/*        <Footer />*/}
+            {/*    </div>*/}
+            {/*</footer>*/}
+            <UserLogIn />
         </div>
     )
 }
