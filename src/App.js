b489fba67319902e7d32b1167f1ad277ab4ae09f
lisachenko_roman <lisachenkoroma2009@gmail.com>
2023-04-26 12:56:37 +0300

    внесены правки 26.04.2023

--- src/App.js
+++ src/App.js
@@ -15,31 +15,30 @@ import UserLogIn from "./components/UserLogIn/UserLogIn";
 function App() {
     return (
         <div className="App">
-            {/*<div className='wrapper'>*/}
-            {/*    <header className='header'>*/}
-            {/*        <Navbar />*/}
-            {/*        <div className='searchContainer'>*/}
-            {/*            <Search />*/}
-            {/*            <User />*/}
-            {/*        </div>*/}
-            {/*    </header>*/}
-            {/*        <main>*/}
-            {/*            <Greetings />*/}
-            {/*            <Why />*/}
-            {/*            <div className='circle'></div>*/}
-            {/*            <Stock />*/}
-            {/*            <Reviews />*/}
-            {/*            <Question />*/}
-            {/*            <Cooperation />*/}
-            {/*            <InvitationToCooperation />*/}
-            {/*        </main>*/}
-            {/*</div>*/}
-            {/*<footer>*/}
-            {/*    <div className='wrapper'>*/}
-            {/*        <Footer />*/}
-            {/*    </div>*/}
-            {/*</footer>*/}
-            <UserLogIn />
+            <div className='wrapper'>
+                <header className='header'>
+                    <Navbar />
+                    <div className='searchContainer'>
+                        <Search />
+                        <User />
+                    </div>
+                </header>
+                    <main>
+                        <Greetings />
+                        <Why />
+                        <div className='circle'></div>
+                        <Stock />
+                        <Reviews />
+                        <Question />
+                        <Cooperation />
+                        <InvitationToCooperation />
+                    </main>
+            </div>
+            <footer>
+                <div className='wrapper'>
+                    <Footer />
+                </div>
+            </footer>
         </div>
     )
 }
